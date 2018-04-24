2540b5585c4921f17426c62352097d43c469a56f
Babu hussain <babuhussain@bwdev.local>
2018-04-24 22:09:20 +0530

    registaration page chnages submit

--- assets/js/registration.js
+++ assets/js/registration.js
@@ -62,7 +62,7 @@ jQuery(document).ready(function($) {
         else {
             $('input[name=plugin-type]').parents('div.spnx-box-reg-cmn-cls').find('div.error-cmn-clas-spnx').css('display', 'none');
         }
-        $("#bussiness-name-spnx, #street-ad-bs-spnx, #city-bs-spnx, #zip_code_number, #state-ad-bs-spnx, #phone-ad-bs-spnx, #paypal-bs-spnx").trigger('blur');
+        $("#bussiness-name-spnx, #street-ad-bs-spnx, #city-bs-spnx, #zip_code_number, #state-ad-bs-spnx, #phone-ad-bs-spnx").trigger('blur');
         if($('.error-cmn-clas-spnx').is(':visible') || flag) {
             return false;
         }
@@ -91,7 +91,6 @@ jQuery(document).ready(function($) {
             }
         }
         else {
-            $(this).next().css('display', 'block');
         }
     });
     $("#zip_code_number").blur(function () {
@@ -115,6 +114,7 @@ jQuery(document).ready(function($) {
         }
 
     });
+
 
     function Validateemail(email) {
         var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
