92b5d559e79b3d90f38f6fb2d1230d08a7a0dbf0
babuhussain <babu.hussain@brandwiki.today>
2018-08-02 12:46:31 +0530

    logo link target and css change

--- resources/views/admin/spinkx-css-js-enqueue.php
+++ resources/views/admin/spinkx-css-js-enqueue.php
@@ -399,7 +399,7 @@ function spinkx_header_menu() {
 	$settings = maybe_unserialize( $settings );
 	$site_id = isset($settings['site_id'])?$settings['site_id']:0;
     $settings['due_date'] = isset($settings['due_date'])?$settings['due_date']:'0000-00-00 00:00:00';
-    ?><div><div class="spnx-menu-logo"><span><a href="https://www.spinkx.com"><img src="<?php echo SPINKX_CONTENT_DIST_URL; ?>images/spinkx-logo.png" /></a></span></div>
+    ?><div><div class="spnx-menu-logo"><span><a target="_blank" href="https://www.spinkx.com"><img src="<?php echo SPINKX_CONTENT_DIST_URL; ?>images/spinkx-logo.png" /></a></span></div>
 	<ul class="nav nav-tabs">
         <?php if(  $settings['due_date'] == '0000-00-00 00:00:00'  ) {?>
 		<li <?php echo ('spinkx-site-register' === $page)?'class="active"':''?>><a href="?page=spinkx-site-register">Registration</a></li>
