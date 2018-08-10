ed4486307e03267ac206bf94f31106897874f4b4
Vikash Saharan <vikashpkl@gmail.com>
2018-08-10 18:31:11 +0530

    js remove dashboard

--- resources/views/admin/dashboard/site-dashboard.php
+++ resources/views/admin/dashboard/site-dashboard.php
@@ -6,18 +6,5 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
             <iframe id="landing_url" width="100%" height="1440" src="https://www.spinkx.com/landing_page_url/" >
             </iframe>
         </div>
-    </div>
-
-<script>
-    jQuery(document).ready(function($){
-        $('#landing_url').load(function () {
-            console.log($(".dsh_cntnt_sub_hldr").find());
-            /*if (typeof callback == 'function') {
-                callback($('body', this.contentWindow.document).html());
-            }
-            setTimeout(function () {$('#frameId').remove();}, 50);*/
-        });
-     // console.log($("#landing_url").contents());
-    });
-</script>
+</div>
 
