070d660bc780aff6460dcc2ebf38d99735c32f35
Vikash Saharan <vikashpkl@gmail.com>
2018-08-10 15:16:38 +0530

    changes

--- resources/views/admin/dashboard/site-dashboard.php
+++ resources/views/admin/dashboard/site-dashboard.php
@@ -3,7 +3,7 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 ?>
 <div class="dsb_mn_cntr_spx">
     <div class="wdgt_cntnt_sub_hldr dsh_cntnt_sub_hldr">
-            <iframe id="landing_url" width="100%" height="1410" src="https://www.spinkx.com/landing_page_url/3">
+            <iframe id="landing_url" width="100%" height="1410" src="https://www.spinkx.com/landing_page_url/">
             </iframe>
 
 
